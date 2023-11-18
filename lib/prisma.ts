560554ffbdba18261e0eb3b7105214317b81aedf
zachsiegel <zsiegel92@gmail.com>
2023-11-18 11:17:13 -0800

    prisma withAccelerate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 declare global {
-  var prisma: PrismaClient | undefined;
+    var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient();
+const prisma = global.prisma || new PrismaClient().$extends(withAccelerate());
+
 
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
