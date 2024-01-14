840e676e54f04d310d001396ae791ece3d80fae4
zachsiegel <zsiegel92@gmail.com>
2024-01-13 16:23:13 -0800

    remove db push from build

--- prisma_migration_setup.sh
+++ prisma_migration_setup.sh
@@ -1,4 +1,3 @@
 mkdir -p prisma/migrations/0_init
- npx prisma migrate diff --from-empty --to-schema-datamodel prisma/schema.prisma --s
-cript > prisma/migrations/0_init/migration.sql
+npx prisma migrate diff --from-empty --to-schema-datamodel prisma/schema.prisma --script > prisma/migrations/0_init/migration.sql
 npx prisma migrate resolve --applied 0_init 
