eac8a178052d2679e0f6918c6ff6f70b6716af28
zachsiegel <zsiegel92@gmail.com>
2024-01-15 20:47:38 -0800

    get token in Python

--- api/app.py
+++ api/app.py
@@ -1,9 +1,17 @@
-from pydantic import BaseModel, Field
+import os
 from datetime import datetime
-# from prisma import Prisma
-from .FastRPC import FastRPC
+
+from pydantic import BaseModel, Field
+from typing import Annotated, Union
+
 from prisma import Prisma
 from prisma.models import Capsule, User
+
+from passlib.context import CryptContext
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+from .FastRPC import FastRPC
 
 app = FastRPC(prefix='/api/py')
 
@@ -31,13 +39,20 @@ async def hello3(message: str) -> MessageData:
     return MessageData(message=message)
 
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+ALGORITHM = "HS256"  # "HS512"  #"HS256" # next-auth uses "A256GCM" by default
+SECRET_KEY = os.environ.get('NEXTAUTH_SECRET')
+
+
 @app.RPC
-async def getUser() -> User:
+async def getUser(token: Annotated[str, Depends(oauth2_scheme)], ) -> User:
     '''Retuns a message depending on the route.'''
     prisma = Prisma()
     await prisma.connect()
     user = await prisma.user.find_first(where={'id': 1})
     print(f'PRINTING USER FROM PYTHON: {user}')
+    print(token)
     # user = User(id=1, name='test', email='test', password='test')
     # print(f'PRINTING USER FROM PYTHON: {user}')
     return user
